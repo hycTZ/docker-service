ad54f93461a5d619d84cd763c65bea82ef6135ea
lihe <li.he1@zte.com.cn>
2016-11-21 15:22:31 +0800

    增加spring cloud bus相关内容

--- spring-cloud-2.0/add-service-demo/src/main/java/demo/controller/AddController.java
+++ spring-cloud-2.0/add-service-demo/src/main/java/demo/controller/AddController.java
@@ -1,5 +1,7 @@
 package demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class AddController {
+
+    @Value("${my.info.str}")
+    private String infoStr;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Map<String, Object> add(Integer a, Integer b) {
         System.out.println("端口为8100的实例被调用");
+        System.out.println("infoStr : " + infoStr);
         Map<String, Object> returnMap = new HashMap<>();
         returnMap.put("code", 200);
         returnMap.put("msg", "操作成功");
